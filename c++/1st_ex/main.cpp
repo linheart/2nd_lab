2727f8071c75140e8202f758deacf7f2d77e0401
Alexei <leshamihaylenko@bk.ru>
2024-04-04 12:29:29 +0700

    1st ex c++

---
+++ c++/1st_ex/main.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int init(int val);
+int func(int S, int M, int N);
+
+int main() {
+
+  int S, M, N;
+
+	S = init(S);
+	N = init(N);
+	M = init(M);
+
+	cout << func(S, M, N) << endl;
+  return 0;
+}
+
+int init(int val) {
+	if	(!(cin >> val)) {
+		cerr << "Invalid input";
+		exit(EXIT_FAILURE);
+	}
+	
+	return val;
+}
+
+int func(int S, int M, int N) {
+  if (S * M > (S - S / 7) * N) {
+    return -1;
+  }
+  return ceil((double)S * M / N);
+}
