8abca922e394ce60d7e39f4f2cdd5dc3636f28c8
Alexei <leshamihaylenko@bk.ru>
2024-04-04 13:58:04 +0700

    update 3d ex python

--- python/3d_ex/main.py
+++ python/3d_ex/main.py
@@ -1,12 +1,22 @@
+def err(exc):
+    raise Exception(exc)
+
+def init():
+    return int(input())
+
 def counting_dup(n):
-    a = b = int(input())
+    a = b = init()
     count = 0
 
     for _ in range(n - 1):
-        a = int(input())
+        a = init()
         if a == b:
             count += 1
         b = a
     return count
 
-print(counting_dup(int(input())))
+n = init()
+if n <= 0:
+    err("Invalid input")
+
+print(counting_dup(n))
