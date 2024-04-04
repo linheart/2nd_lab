37e2fdc7f93aad93613437647bb9e745c723d708
Alexei <leshamihaylenko@bk.ru>
2024-04-04 16:32:52 +0700

    update 2nd ex rust

--- rust/2nd_ex/main.rs
+++ rust/2nd_ex/main.rs
@@ -1,6 +1,6 @@
 use std::io::stdin;
 
-fn counting_steps(arr: &Vec<i32>, v: i32) -> i32 {
+fn counting_steps(arr: &Vec<i32>, v: i32) -> usize {
     let mut count = 0;
     let mut tmp = v;
 
@@ -16,30 +16,58 @@ fn counting_steps(arr: &Vec<i32>, v: i32) -> i32 {
     count
 }
 
-fn main() {
+fn init_str() -> String {
     let mut input = String::new();
-    
     stdin().read_line(&mut input).expect("Failed to read line");
-    let n: usize = input.trim().parse().expect("Input was not a number");
-    input.clear();
-    
-    stdin().read_line(&mut input).expect("Failed to read line");
+
+    input
+}
+
+fn init_i32(input: String) -> i32 {
+    match input.trim().parse::<i32>() {
+        Ok(num) if num > 0 => return num,
+        _ => panic!("Invalid input"),
+    }
+}
+
+fn init_vec(input: String) -> Vec<i32> {
     let arr: Vec<i32> = input
         .split_whitespace()
-        .map(|s| s.parse().unwrap())
+        .map(|s| {
+            s.parse::<i32>().unwrap_or_else(|_| {
+                panic!("Invalid input");
+            })
+        })
+        .filter(|&n| {
+            if n > 0 {
+                true
+            } else {
+                panic!("Invalid input");
+            }
+        })
         .collect();
-    if arr.len() != n {
-        panic!("The array does not consist of n elements");
-    }
-    input.clear();
-    
-    stdin().read_line(&mut input).expect("Failed to read line");
-    let v: i32 = input.trim().parse().expect("Input was not a number");
-    
+
+    arr
+}
+
+fn cmp(v: i32, arr: &Vec<i32>) {
     if v < *arr.iter().max().unwrap() {
         panic!("The volume of the watering can is not enough to water the flower");
     }
-    
-    println!("{}", counting_steps(&arr, v));
 }
 
+fn main() {
+    let n = init_i32(init_str());
+
+    let arr: Vec<i32> = init_vec(init_str());
+
+    if arr.len() != (n as usize) {
+        panic!("The array does not consist of n elements");
+    }
+
+    let v = init_i32(init_str());
+
+    cmp(v, &arr);
+
+    println!("{}", counting_steps(&arr, v));
+}
