5d5be920f76082d8e7d7e26cf89a8aa72482f66c
Linheart <leshamihaylenko@bk.ru>
2024-11-05 09:24:00 +0700

    2nd_ex

--- 2nd_ex/main.cpp
+++ 2nd_ex/main.cpp
@@ -2,4 +2,28 @@
 
 using namespace std;
 
-int main() { return 0; }
+int main() {
+  Set<string> set1;
+  set1.insert("asd");
+
+  Set<string> set2;
+  set2.insert("lol");
+  set2.insert("kek");
+
+  Set<string> *set = setUnion(set1, set2);
+
+  for (size_t i = 0; i < 100; i++) {
+    if (set->node[i]) {
+      Node<string> *cur = set->node[i];
+      while (cur) {
+        cout << cur->value << " ";
+        cur = cur->next;
+      }
+      cout << endl;
+    }
+  }
+
+  delete set;
+
+  return 0;
+}
